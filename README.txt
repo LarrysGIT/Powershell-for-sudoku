5bb14c6c7fddb0c74ff03aac2bd5189c34045798
Larry Song <larrysong@LongsMacBookPro.gateway>
2017-04-15 22:54:00 +1000

    first commit

---
+++ README.txt
@@ -0,0 +1,33 @@
+# Powershell-for-sudoku
+
+# just for fun
+# this is a powershell script to solve sudoku
+
+# how to use?
+# please open sudoku.txt for example
+
+PS> . .\sudoku.ps1
+PS> Solve-Sudoku -HowManyAnswersYouWanttoGet 2
+Sudoku array count: 1
+Answer:
+2 1 4 8 7 5 6 9 3
+1 8 5 2 3 6 9 4 7
+3 5 1 6 2 7 8 9 4
+6 3 9 5 4 8 1 7 2
+9 7 2 1 6 3 4 5 8
+8 6 7 9 5 4 2 3 1
+5 4 6 7 8 1 3 2 9
+7 9 3 4 1 2 6 8 5
+4 2 8 3 9 5 7 1 6
+Answer:
+2 1 4 9 7 5 6 9 3
+1 8 5 2 3 6 9 4 7
+3 5 1 6 2 7 8 9 4
+6 3 9 5 4 8 1 7 2
+9 7 2 1 6 3 4 5 8
+8 6 7 9 5 4 2 3 1
+5 4 6 7 8 1 3 2 9
+7 9 3 4 1 2 6 8 5
+4 2 8 3 9 5 7 1 6
+
+ - Larry.Song@outlook.com
---
+++ sudoku.ps1
@@ -0,0 +1,164 @@
+
+function Solve-Sudoku
+{
+
+    param(
+        [string]$Path = 'sudoku.txt',
+        # How many answers to return?
+        [int]$HowManyAnswersYouWanttoGet = 1
+    )
+
+    try
+    {
+        $SudokuMatrix = Get-Content -Path $Path
+    }
+    catch
+    {
+        Write-Host 'Please provide sudoku text file'
+        return
+    }
+
+    $SudokuMatrix = $SudokuMatrix | ?{$_ -notmatch '^#'}
+    $SudokuMatrix = ($SudokuMatrix -join '') -ireplace '\s'
+
+    $SudokuMatrix = [regex]::Matches($SudokuMatrix, '\d{81}')
+
+    if(!$SudokuMatrix)
+    {
+        Write-Host "No enough numbers found in file: $Path"
+        Write-Host 'At least 81 numbers?'
+        return
+    }
+
+    $SudokuMatrixs = @()
+    $SudokuMatrix | %{
+        $SudokuMatrixs += $null
+        $SudokuMatrixs[-1] = @()
+        [regex]::Matches($_.Value, '\d{9}') | %{
+            $SudokuMatrixs[-1] += $null
+            $SudokuMatrixs[-1][-1] = @([regex]::Matches($_.Value, '\d') | %{[int]$_.Value})
+        }
+    }
+
+    Write-Host "Sudoku array count: $($SudokuMatrixs.Count)"
+
+    function GoCalculate($arr){
+        # update array to the latest
+        GoLoop($arr)
+        # verify the calculation is not a dead end!
+        $ZeroPosition = Verify($arr)
+        if($ZeroPosition[2]){
+            # Write-Host "0 option found: [$($ZeroPosition[0])][$($ZeroPosition[1])]"
+            return
+        }
+        # how many cells confirmed
+        if((CellsConfirmed($arr)) -eq 81){
+            $Script:AnswerCount++
+            $Script:Results += $null
+            $Script:Results[-1] = @($arr | %{@($_ | %{$_[0]}) -join ' '})
+            return
+        }
+        # find the first cell with the leatest options
+        $TheLeatestCell = FindTheLeatest($arr)
+        $Options = @($arr[$TheLeatestCell[0]][$TheLeatestCell[1]]).PSObject.Copy()
+        #Write-Host "Row: $($TheLeatestCell[0]); Col: $($TheLeatestCell[1]); Option: $($Options -join ' ')"
+        foreach($Option in $Options){
+            # Assume an option to the cell go a new loop
+            $arr[$TheLeatestCell[0]][$TheLeatestCell[1]] = @($Option)
+            if($AnswerCount -lt $HowManyAnswersYouWanttoGet){
+                GoCalculate($arr)
+            }
+            else
+            {return}
+        }
+    }
+
+    # Loop each cell, calculate possible options and remove impossible options from cell.
+    function GoLoop($arr){
+        #$NewArr = @($null) * 9
+        #for($i = 0; $i -lt 9; $i++){
+        #    $NewArr[$i] = $arr[$i].PSObject.Copy()
+        #}
+        for($i = 0; $i -lt 9; $i++){
+            for($j = 0; $j -lt 9; $j++){
+                if($arr[$i][$j].Count -ne 1){
+                    for($k = 0; $k -lt 9; $k++){
+                        if($arr[$i][$k].Count -eq 1 -and $arr[$i][$j].Count -ne 1){
+                            $arr[$i][$j] = @($arr[$i][$j] | ?{$_ -ne $arr[$i][$k][0]})
+                        }
+                        if($arr[$k][$j].Count -eq 1 -and $arr[$i][$j].Count -ne 1){
+                            $arr[$i][$j] = @($arr[$i][$j] | ?{$_ -ne $arr[$k][$j][0]})
+                        }
+                    }
+                }
+            }
+        }
+        #return $NewArr
+    }
+
+    # Find the cell with the least number options, return its position and count of options
+    function FindTheLeatest($arr){
+        foreach($k in 2..9){
+            for($i = 0; $i -lt 9; $i++){
+                for($j = 0; $j -lt 9; $j++){
+                    if($arr[$i][$j].Count -eq $k){
+                        return $i, $j, $k
+                    }
+                }
+            }
+        }
+    }
+
+    # Calculate how many cells have been confirmed, if it's 81, correct answer hit.
+    function CellsConfirmed($arr){
+        $Confirmed = 0
+        for($i = 0; $i -lt 9; $i++){
+            for($j = 0; $j -lt 9; $j++){
+                if($arr[$i][$j].Count -eq 1){
+                    $Confirmed++
+                }
+            }
+        }
+        return $Confirmed
+    }
+
+    # Loop each cell, if the number options of the cell is null, means current loop is a dead end.
+    function Verify($arr){
+        for($i = 0; $i -lt 9; $i++){
+            for($j = 0; $j -lt 9; $j++){
+                if($arr[$i][$j].Count -eq 0){
+                    return $i, $j, $true # dead end
+                }
+            }
+        }
+        return $i, $j, $false
+    }
+
+    foreach($SudokuMatrix in $SudokuMatrixs)
+    {
+        # Loop each cell, add array [1..9] for each null cell.
+        for($i = 0; $i -lt 9; $i++){
+            for($j = 0; $j -lt 9; $j++){
+                if(!$SudokuMatrix[$i][$j]){
+                    $SudokuMatrix[$i][$j] = 1..9
+                }else{
+                    $SudokuMatrix[$i][$j] = @($SudokuMatrix[$i][$j])
+                }
+            }
+        }
+
+        $Script:AnswerCount = 0
+        $Script:Results = @()
+
+        # Trigger
+        GoCalculate($SudokuMatrix)
+
+        # Output answers
+        $Results | %{
+            if($_ -eq $null){return}
+            Write-Host 'Answer:' -ForegroundColor Yellow
+            $_ -join "`n"
+        }
+    }
+
+}
